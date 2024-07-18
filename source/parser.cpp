206b7c09906d650a434ce36717e189d20f917b57
Rudy Fisher <rfisherd7@gmail.com>
2024-07-17 23:33:42 -0500

    added hello world koi script

--- source/parser.cpp
+++ source/parser.cpp
@@ -12,6 +12,10 @@ namespace Scripting {
 Parser::Error Parser::parse(const std::vector<Token>& tokens, AstNode** out_ast) {
     Error result = SCRIPTING_PARSER_ERROR_OK;
 
+    //todo::
+    // 1. build out ast
+    // 2. register ids from declarations found in program
+
     //fixme:: validate that exe<main>({}); is the first few tokens
     *out_ast = new Execute(0u);
     AstNode* root = *out_ast;
