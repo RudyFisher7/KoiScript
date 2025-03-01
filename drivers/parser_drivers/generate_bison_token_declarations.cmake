4e5bcb0f196258ec912bd5e118c3d7b2dae4cdb5
Rudy Fisher <rfisherd7@gmail.com>
2025-03-01 16:03:13 -0600

    Finished script for making yacc tokens from enum

--- drivers/parser_drivers/generate_bison_token_declarations.cmake
+++ drivers/parser_drivers/generate_bison_token_declarations.cmake
@@ -3,32 +3,67 @@ cmake_minimum_required(VERSION 3.13)
 
 set(EnumFilePath "../common/include/drivers_common/token_type.h")
 
+message(STATUS "\n\n-- USAGE***********************")
+message(STATUS "First enum value must be assigned a value (e.g., 'MY_ENUM_VALUE = 256,').")
+message(STATUS "Only the first enum may be assigned a value, the rest will be enumerated by this script.")
+message(STATUS "The last enum value must end with 'SIZE'.")
+message(STATUS "USAGE***********************\n\n")
+message(STATUS "Loading: ${EnumFilePath}")
+
 file(READ ${EnumFilePath} EnumFile)
 
 string(REPLACE "\n" ";" FileLines ${EnumFile})
 
+message(STATUS "Parsing enum for tokens...")
+
 set(StartLine "")
 set(i 0)
 list(LENGTH FileLines LinesSize)
-while(StartLine STREQUAL "" AND i LESS LinesSize)
+while (StartLine STREQUAL "" AND i LESS LinesSize)
     list(GET FileLines ${i} Line)
     if (Line MATCHES "{$")
         set(StartLine ${Line})
-    endif()
+    endif ()
     MATH(EXPR i ${i}+1)
-endwhile()
+endwhile ()
 
-message(STATUS ${StartLine})
+list(GET FileLines ${i} StartLine)
+string(REGEX MATCH "[0-9]+" TokenValue "${StartLine}")
+message(STATUS "Starting at enum value: ${StartLine}")
+if (TokenValue)
+    message(STATUS "First value: ${TokenValue}")
+endif ()
 
 set(Tokens "")
 
 set(EndLine "")
-while(EndLine STREQUAL "" AND i LESS LinesSize)
+while (EndLine STREQUAL "" AND i LESS LinesSize)
     list(GET FileLines ${i} Line)
+
+    string(STRIP "${Line}" StrippedLine)
+
+    string(FIND "${StrippedLine}" " " SpaceIndex)
+    string(FIND "${StrippedLine}" "," CommaIndex)
+
+    if (NOT SpaceIndex EQUAL -1)
+        string(SUBSTRING "${StrippedLine}" 0 ${SpaceIndex} EnumString)
+    elseif (NOT CommaIndex EQUAL -1)
+        string(SUBSTRING "${StrippedLine}" 0 ${CommaIndex} EnumString)
+    else ()
+        set(EnumString "${StrippedLine}")
+    endif ()
+
+    list(APPEND Tokens "%token ${EnumString} ${TokenValue}\n")
+
+    MATH(EXPR TokenValue ${TokenValue}+1)
+
     if (Line MATCHES "SIZE$")
         set(EndLine ${Line})
-    endif()
+    endif ()
     MATH(EXPR i ${i}+1)
-endwhile()
+endwhile ()
 
-message(STATUS ${EndLine})
+message(STATUS "Generating file koiscript_tokens.gen.txt...")
+
+file(WRITE koiscript_tokens.gen.txt ${Tokens})
+message(STATUS "Done.")
