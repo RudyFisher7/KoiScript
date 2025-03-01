d336ab10066cc0081075fb8c87a7716d82c8415a
Rudy Fisher <rfisherd7@gmail.com>
2025-03-01 15:22:23 -0600

    working on generation script some more

--- drivers/parser_drivers/generate_bison_token_declarations.cmake
+++ drivers/parser_drivers/generate_bison_token_declarations.cmake
@@ -1,11 +1,34 @@
+cmake_minimum_required(VERSION 3.13)
+
+
 set(EnumFilePath "../common/include/drivers_common/token_type.h")
 
 file(READ ${EnumFilePath} EnumFile)
 
 string(REPLACE "\n" ";" FileLines ${EnumFile})
 
-foreach(Line IN LISTS FileLines)
-    if (Line MATCHES "MIN,$")
-        message(STATUS ${Line})
+set(StartLine "")
+set(i 0)
+list(LENGTH FileLines LinesSize)
+while(StartLine STREQUAL "" AND i LESS LinesSize)
+    list(GET FileLines ${i} Line)
+    if (Line MATCHES "{$")
+        set(StartLine ${Line})
     endif()
-endforeach()
+    MATH(EXPR i ${i}+1)
+endwhile()
+
+message(STATUS ${StartLine})
+
+set(Tokens "")
+
+set(EndLine "")
+while(EndLine STREQUAL "" AND i LESS LinesSize)
+    list(GET FileLines ${i} Line)
+    if (Line MATCHES "SIZE$")
+        set(EndLine ${Line})
+    endif()
+    MATH(EXPR i ${i}+1)
+endwhile()
+
+message(STATUS ${EndLine})
